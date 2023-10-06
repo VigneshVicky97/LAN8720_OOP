92d7ec25ddbddac4a8bf49e09f0de39011e65db7
vignesh <vigneshs@melss.com>
2023-10-06 16:44:52 +0530

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#define DEBUG_ETHERNET_WEBSERVER_PORT       Serial
+
+// Debug Level from 0 to 4
+#define _ETHERNET_WEBSERVER_LOGLEVEL_       3
+
+#include <WebServer_WT32_ETH01.h>
+
+WebServer server(80);
+
+// Select the IP address according to your local network
+IPAddress myIP(192, 168, 1, 232);
+IPAddress myGW(192, 168, 1, 1);
+IPAddress mySN(255, 255, 255, 0);
+
+// Google DNS Server IP
+IPAddress myDNS(8, 8, 8, 8);
+
+void handleRoot()
+{
+  String html = F("Hello from HelloServer running on ");
+
+  html += String(BOARD_NAME); 
+ 
+  server.send(200, F("text/plain"), html);
+}
+
+void handleNotFound()
+{
+  String message = F("File Not Found\n\n");
+  
+  message += F("URI: ");
+  message += server.uri();
+  message += F("\nMethod: ");
+  message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
+  message += F("\nArguments: ");
+  message += server.args();
+  message += F("\n");
+  
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  
+  server.send(404, F("text/plain"), message);
+}
+
+void setup()
+{   
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.print("\nStarting HelloServer on " + String(ARDUINO_BOARD));
+  Serial.println(" with " + String(SHIELD_TYPE));
+  Serial.println(WEBSERVER_WT32_ETH01_VERSION);
+
+  // To be called before ETH.begin()
+  WT32_ETH01_onEvent();
+
+  ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
+
+  // Static IP, leave without this line to get IP via DHCP
+  //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
+  ETH.config(myIP, myGW, mySN, myDNS);
+
+  WT32_ETH01_waitForConnect();
+
+  server.on(F("/"), handleRoot);
+
+  server.on(F("/inline"), []() 
+  {
+    server.send(200, F("text/plain"), F("This works as well"));
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+
+  Serial.print(F("HTTP EthernetWebServer is @ IP : "));
+  Serial.println(ETH.localIP());
+}
+
+void loop()
+{
+  server.handleClient();
+}
